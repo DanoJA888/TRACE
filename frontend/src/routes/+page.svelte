3827c4ca005661bd24c8bc53af326d843ed3a0b2
DanoJA888 <djaguilargzz01@gmail.com>
2025-03-21 12:03:13 -0600

    very basic crawler config, not well structured, need folders for each 'tool' or set of pages for each tool

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let crawlerParams = [
+    { id: "target-url", label: "Target URL", type: "text"},
+		{ id: "depth", label: "Crawl Depth", type: "number"},
+		{ id: "max-pages", label: "Max Pages", type: "number"},
+		{ id: "user-agent", label: "User Agent", type: "text"},
+		{ id: "delay", label: "Request Delay", type: "number"},
+		{ id: "proxy", label: "Proxy", type: "number"}
+];
+</script>
+
+<div class = crawlerConfigPage>
+  <div>
+    <h1>Crawler</h1>
+    <div >
+      <form on:submit|preventDefault={handleSubmit}>
+        {#each crawlerParams as param}
+          <label>
+            {param.label}:
+            {#if param.type === 'textarea'}
+              <textarea bind:value={crawlerParams[param.key]} required></textarea>
+            {:else}
+              <input type={param.type} bind:value={crawlerParams[param.key]} required />
+            {/if}
+          </label>
+        {/each}
+        
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    
+  </div>
+</div>
+  
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    width: 300px;
+    margin: 50px auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  }
+
+  label {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    font-weight: bold;
+  }
+
+</style>
+
