30e7f24735f0cf69c0e04460ce9679d483bee8bc
LeonG19 <lgarza3@miners.utep.edu>
2025-03-28 12:58:28 -0600

    Trying to fix credentials generation

--- backend/AI_backend_interface.py
+++ backend/AI_backend_interface.py
@@ -5,7 +5,8 @@ import logging
 from typing import Optional
 import shutil
 import os
-from mdp3 import CredentialGeneratorMDP
+import mdp3
+from mdp3 import CredentialGeneratorMDP, WebScraper, CredentialMDP
 from typing import Dict, Optional
 import json
 
@@ -68,12 +69,17 @@ async def generate_credentials_endpoint(file: UploadFile = File(None), data: str
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
-    print("second")
-    print(data)
-    print(file_word)
+
+
+    urls = mdp3.load_urls_from_csv("site_list.csv")
+    csv_path = "/csv_uploads/web_text.csv"
+    scrapper = WebScraper(urls)
+    scrapper.generate_csv(csv_path)
+    mdp3.nlp_subroutine(csv_path)
+
     data = json.loads(data)
     generator = CredentialGeneratorMDP(
-        csv_path= "site_list.csv",
+        csv_path= csv_path,
         wordlist_path= file_word,
         user_include_char = data["userChar"],
         user_include_num = data["userNum"],
@@ -86,6 +92,9 @@ async def generate_credentials_endpoint(file: UploadFile = File(None), data: str
         pass_length = data["passLen"] 
     )
     credentials = generator.generate_credentials(10)
+    print("\nGenerated Credentials:")
+    for username, password in credentials:
+        print(f"Username: {username}, Password: {password}")
     return {"credentials": credentials}
 
     """
